c7539d3d953ca12cbba40fddc4e9bf5631013710
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-11 22:38:29 +0530

    update bot/__init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,1 +1,13 @@
-print ("hello")
+#      iiiiii   nn    nn    ssssss  tttttt    aaaaaaaaa                         
+#        ii     nnn   nn    ss        tt      aa     aa                          
+#        ii     nn n  nn    ssssss    tt      aaaaaaaaa                         
+#        ii     nn  n nn        ss    tt      aa     aa                         
+#      iiiiii   nn   nnn    ssssss    tt      aa     aa         
+
+"""
+Instagram Bot Library
+~~~~~~~~~~~~~~~~~~~~~
+
+This an instagram bot library, written in Python, for human beings.
+"""
+
