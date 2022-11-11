0a9c321c079f7400f6842785744a3c3d635704a9
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-12 04:22:39 +0530

    update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,13 +1,11 @@
 from time import sleep
 from .__insta__ import Bot
-from .__media__ import Media
 from .__analysis__ import Analysis
 
-class Run(Bot,Media,Analysis):
+class Run(Bot,Analysis):
     def __init__(self,username:str,password:str,api_key:str,**kwargs) -> None:
         super().__init__(username,password)
         super(Bot,self).__init__(api_key)
-        super(Media,self).__init__("test")
         self.__run__()
 
     def __sleeper__(self,time:int):
@@ -20,5 +18,5 @@ class Run(Bot,Media,Analysis):
 
     def __run__(self):
         while True:
-
+            super().__analysis__()
             self.__sleeper__(0.001)
