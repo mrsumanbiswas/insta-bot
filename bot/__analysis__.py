2ecd34f194e53e7dc590051358669a7b6d46d0e3
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-12 03:43:06 +0530

    update __analysis__.py

--- bot/__analysis__.py
+++ bot/__analysis__.py
@@ -1,3 +1,9 @@
 class Analysis():
     def __init__(self,text:str) -> None:
         print("hello form analysis "+text)
+
+    def __sleeper__():
+        pass
+
+    def __uploader__():
+        pass
