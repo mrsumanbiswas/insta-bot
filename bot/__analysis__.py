de3211150f92ce49103ab810cffc9a5dece12cc4
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-12 03:22:43 +0530

    update __analysis__.py and __main__.py

---
+++ bot/__analysis__.py
@@ -0,0 +1,3 @@
+class Analysis():
+    def __init__(self,text:str) -> None:
+        print("hello form analysis "+text)
--- bot/__main__.py
+++ bot/__main__.py
@@ -1,9 +1,9 @@
 from .__insta__ import Bot
 from .__media__ import Media
-from .__analysis__ import *
+from .__analysis__ import Analysis
 
-class Run(Bot,Media):
-    def __init__(self,username:str,password:str,api_key:str) -> None:
+class Run(Bot,Media,Analysis):
+    def __init__(self,username:str,password:str,api_key:str,**kwargs) -> None:
         super().__init__(username,password)
         super(Bot,self).__init__(api_key)
-        
+        super(Media,self).__init__("test")
