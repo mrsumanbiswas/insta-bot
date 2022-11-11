dcfe680cb6e6630fdb1df93576389e687132e6c2
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-12 03:28:18 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # from instabot import 
-from bot import *
+from bot import Run
+from os import getenv
 
 if __name__ == "__main__":
-    # Bot(
-    #     username="sdfs",
-    #     password="dfsd"
-    # )
-    pass
+    try:
+        username=getenv("username"),
+        password=getenv("password"),
+        api_key=getenv("api_key")
+
+    except:
+        username="test_user_name",
+        password="test_password",
+        api_key="test_api_key"
+
+    Run(username,password,api_key)
