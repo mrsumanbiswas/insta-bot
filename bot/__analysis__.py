6ed222b17bb5d6b5fe11a156697a21033018fb48
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-12 04:03:32 +0530

    update __analysis__.py

--- bot/__analysis__.py
+++ bot/__analysis__.py
@@ -1,9 +1,14 @@
+from time import sleep
+
 class Analysis():
     def __init__(self,text:str) -> None:
         print("hello form analysis "+text)
 
-    def __sleeper__():
-        pass
+    def __sleeper__(self,time:int):
+        """
+        ### `time` is in hour(s)
+        """
+        sleep(time*60*60)
 
-    def __uploader__():
+    def __uploader__(self):
         pass
