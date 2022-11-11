8dc4436b2030d2949f4f5f668313fc7b182792eb
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-11 23:59:14 +0530

    update insta.py

--- bot/insta.py
+++ bot/insta.py
@@ -10,10 +10,37 @@ class Bot():
         self.bot = insta()
         # self.bot.login(username=username,password=password)
 
-    def upload_image():
-        pass
-    def upload_video():
-        pass
+    def upload_image(
+        self,
+        photo:str,
+        caption:str,
+    ):
+       """
+        Upload photo to Instagram
+
+        @param photo Path to photo file (String)
+
+        @param caption Media description (String)
+       """
+       return self.bot.upload_photo(photo,caption,None,False)
+
+    def upload_video(self,video:str,caption:str,thumbnail:str):
+        """
+        Upload video to Instagram
+
+        @param video Path to video file (String)
+
+        @param caption Media description (String)
+
+        @param thumbnail Path to thumbnail for video (String). 
+        """
+        return self.bot.upload_video(video,caption,thumbnail)
+
+    def upload_story(self,photo:str):
+        """
+        Upload story photo to Instagram
+        """
+        return self.bot.upload_story_photo(photo)
+
     def follow():
         pass
-    
