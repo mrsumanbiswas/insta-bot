d80f82094d95eda0fa57c3b8f3f9e354fb87e01a
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-12 03:17:10 +0530

    update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,5 +3,7 @@ from .__media__ import Media
 from .__analysis__ import *
 
 class Run(Bot,Media):
-    def __init__(self,username:str,password:str) -> None:
+    def __init__(self,username:str,password:str,api_key:str) -> None:
         super().__init__(username,password)
+        super(Bot,self).__init__(api_key)
+        
