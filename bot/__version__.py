890c45369c6c838a2d28caa152087c677d985d09
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-11 22:27:57 +0530

    update 1 file, delete 1 file and create 1 file

---
+++ bot/__version__.py
@@ -0,0 +1,8 @@
+__title__ = "bot"
+__description__ = "An instagram bot"
+__url__ = "https://github.com/mrsumanbiswas/insta-bot"
+__version__ = "0.00.1"
+__author__ = "Suman Biswas"
+__author_email__ = "mrsumanbiswas0@gmail.com"
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright 2022 mrsumanbiswas"
