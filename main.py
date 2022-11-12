7a8c9f725023789824f80592895125573c9fb865
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-12 17:41:01 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from os import getenv
+from os import getenv,system
 from bot import Run
 
 if __name__ == "__main__":
+    system("rm -r config/*")
     try:
         username=getenv("username"),
         password=getenv("password"),
         api_key=getenv("api_key")
+        Run(username[0],password[0],api_key)
 
     except Exception as e:
         print("error: ",e)
     
-    Run(username[0],password[0],api_key)
     
