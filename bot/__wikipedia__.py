1dd419724ec329b06870f1fd97d82dac400b9a89
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-12 17:37:27 +0530

    update __wikipedia__.py

--- bot/__wikipedia__.py
+++ bot/__wikipedia__.py
@@ -3,7 +3,7 @@ import wikipedia
 
 def Wikipedia(quary: str) -> str:
     try:
-        Summary = wikipedia.summary(quary, sentences=3)
+        Summary = wikipedia.summary(quary, sentences=5)
         texts2 = re.sub("\[.*?\]", "", Summary)
         texts3 = re.sub("\(.*?\)", "", texts2)
         Summary = texts3.replace(")", "").replace("(", "").replace("=", "")
