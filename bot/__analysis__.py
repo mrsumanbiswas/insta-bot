dcef1542a74203e7b40494a458f42d47df21f168
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-12 05:46:38 +0530

    update __analysis__.py and __main__.py

--- bot/__analysis__.py
+++ bot/__analysis__.py
@@ -4,8 +4,11 @@ class Analysis():
     def __init__(self) -> None:
         print("ok")
   
-    def __algo__(self)->list:
-        return ['']
+    def __algo__(self)->dict:
+        print("woo the last part")
+        return {
+            "type":"none"
+        }
 
     def __analysis__(self,url:str,q:str,_type:str)->tuple[str,str,list]:
         content:str = Wikipedia(q)
--- bot/__main__.py
+++ bot/__main__.py
@@ -5,7 +5,7 @@ from .__media__  import Media
 from .__analysis__ import Analysis
 
 class Run(Bot,Media,Analysis):
-    def __init__(self,username:str,password:str,api_key:str,**kwargs) -> None:
+    def __init__(self,username:str,password:str,api_key:str) -> None:
         super().__init__(username,password)
         super(Bot,self).__init__(api_key)
         super(Media,self).__init__()
