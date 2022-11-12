e827a9dd43b4a2a5f31a8e32bc0c779e5e44d9d0
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-12 15:42:02 +0530

    update __analysis__.py

--- bot/__analysis__.py
+++ bot/__analysis__.py
@@ -1,18 +1,43 @@
+from datetime import datetime
 from .__wikipedia__ import Wikipedia
 
 class Analysis():
     def __init__(self) -> None:
-        print("ok")
+        print("I'm ready boss!")
+        self.category =[
+            "backgrounds", "nature", "science", "education", "feelings", "health", "people", "religion", "places", "animals", "industry", "computer", "food", "sports", "transportation", "travel", "buildings", "business", "music"
+        ]
   
     def __algo__(self)->dict:
-        print("woo the last part")
+        hour= datetime.now().hour
+        index = 0
+
+        if hour >= 0 and hour < 4:
+            pass
+        elif hour >= 4 and hour <8:
+            pass
+        elif hour >= 8 and hour <12:
+            pass
+        elif hour >= 12 and hour <16:
+            pass
+        elif hour >= 16 and hour <20:
+            pass
+        else:
+            pass
+        
+        if index == len(self.category):
+            index =0
+        else:
+            index += 1
+        
         return {
-            "type":"none"
+            "type":"all",
+            "category": self.category[index]
         }
 
     def __analysis__(self,url:str,q:str,_type:str)->tuple[str,str,list]:
         content:str = Wikipedia(q)
-        hashtags:list[str] =['auto_insta']
+        hashtags:list[str] =['dizzytechnician']
 
         hashtags.append(q.split(" "))
         content_tag = content.split(" ")
